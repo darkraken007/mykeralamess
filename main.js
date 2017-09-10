c286ce9b9bdad359b1413f540aa7704d2e346d1c
darkraken007 <31367405+darkraken007@users.noreply.github.com>
2017-09-10 18:19:36 +0530

    Testing google auth

--- main.js
+++ main.js
@@ -1,12 +1,62 @@
-var http = require("http");
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Coming Soon!\n');
-}).listen(8080);
+var http = require('http');
+var express = require('express');
+var Session = require('express-session');
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var plus = google.plus('v1');
+const ClientId = "303439398728-3pb3fc30vveotv1b8glfihmplptg9pp0.apps.googleusercontent.com";
+const ClientSecret = "vIyBNFvlBJ2rzJw6YuvS7l1b";
+const RedirectionUrl = "http://www.mykeralamess.com/thankyou";
 
-// Console will print the message
+//starting the express app
+var app = express();
+
+//using session in express
+app.use(Session({
+    secret: 'kraygasm2017#007',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//this is the base route
+app.use("/", function (req, res) {
+    res.send(`
+        &lt;h1&gt;Authentication using google oAuth&lt;/h1&gt;
+    `)
+});
+
+var port = 8080;
+var server = http.createServer(app);
+server.listen(port);
+server.on('listening', function () {
+    console.log(`listening to ${port}`);
+});
+
+
+function getOAuthClient () {
+    return new OAuth2(ClientId ,  ClientSecret, RedirectionUrl);
+}
+
+function getAuthUrl () {
+    var oauth2Client = getOAuthClient();
+    // generate a url that asks permissions for Google+ and Google Calendar scopes
+    var scopes = [
+      'https://www.googleapis.com/auth/plus.me'
+    ];
+
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes // If you only need one scope you can pass it as string
+    });
+
+    return url;
+}
+
+
+app.use("/", function (req, res) {
+    var url = getAuthUrl();
+    res.send(`
+        &lt;h1&gt;Authentication using google oAuth&lt;/h1&gt;
+        &lt;a href=${url}&gt;Login&lt;/a&gt;
+    `)
+});
